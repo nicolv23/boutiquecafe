a756b504e2e09ecd24aaab4fee62ac6a69517e62
Nicolas Lazarte <1853973@crosemont.qc.ca>
2023-09-19 19:22:20 -0400

    Implémentation du projet BoutiqueCafe étant dans un état fonctionnel

---
+++ BoutiqueCafe/Controllers/ProduitController.cs
@@ -0,0 +1,30 @@
+﻿using BoutiqueCafe.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoutiqueCafe.Controllers
+{
+    public class ProduitController : Controller
+    {
+        private IProduitRepository produitRepository;
+        public ProduitController(IProduitRepository produitRepository)
+        {
+            this.produitRepository = produitRepository;
+        }
+
+        public IActionResult Boutique()
+        {
+            return View(produitRepository.GetAllProduits());
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var produit = produitRepository.GetDetailProduit(id);
+            if(produit == null)
+            {
+                return NotFound();
+            }
+            return View(produit);
+        }
+       
+    }
+}
